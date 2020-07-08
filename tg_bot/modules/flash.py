9c8636063446be6b5b04fa602b6e96a42595cabb
Narek Torosyan <narek.email@gmail.com>
2020-07-08 18:05:07 +0400

    Fix flash.py

--- tg_bot/modules/flash.py
+++ tg_bot/modules/flash.py
@@ -7,15 +7,15 @@ from tg_bot import dispatcher
 @run_async
 def flash(bot: Bot, update: Update):
     message = update.effective_message
-        text = message.text[len('/flash '):]
-        r = random.randint(1, 10000)
-		if len(text.split(" ")) > 1:
-			return message.reply_text("`Cannot flash file!`")
-		time.sleep(4)
-		if r % 2 == 1:
-			return message.reply_text(f"`Successfully flashed` {text}.zip`!`")
-		elif r % 2 == 0:
-			return message.reply_text(f"`Flashing` {text}.zip `failed successfully!`")
+    text = message.text[len('/flash '):]
+    r = random.randint(1, 10000)
+    if len(text.split(" ")) > 1:
+        return message.reply_text("`Cannot flash file!`")
+    time.sleep(4)
+    if r % 2 == 1:
+        return message.reply_text(f"`Successfully flashed` {text}.zip`!`")
+    elif r % 2 == 0:
+        return message.reply_text(f"`Flashing` {text}.zip `failed successfully!`")
 
 __help__ = """
 Flash a file. Originally from nunobot++.
