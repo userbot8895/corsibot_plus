bde19f1600e822463635be6284a9979459b99a20
Narek Torosyan <narek.email@gmail.com>
2020-07-09 20:22:21 +0400

    minor fixes in flash.py

--- tg_bot/modules/flash.py
+++ tg_bot/modules/flash.py
@@ -1,5 +1,6 @@
 from telegram import Update, Bot
 from telegram.ext import run_async
+import telegram
 
 from tg_bot.modules.disable import DisableAbleCommandHandler
 from tg_bot import dispatcher
@@ -11,23 +12,23 @@ import time
 def flash(bot: Bot, update: Update):
     message = update.effective_message
     text = message.text[len('/flash '):]
+    message.reply_text(f"`Flashing `{text}.zip`...` ")
+    bot.sendChatAction(update.effective_chat.id, 'TYPING')
     r = random.randint(1, 10000)
     if len(text.split(" ")) > 1:
         return message.reply_text("`Cannot flash file!`")
     time.sleep(4)
     if r % 2 == 1:
-        return message.reply_text(f"`Successfully flashed` {text}.zip`!`")
+        return message.reply_text(f"`Successfully flashed` {text}.zip`!`", telegram.ParseMode.MARKDOWN)
     elif r % 2 == 0:
-        return message.reply_text(f"`Flashing` {text}.zip `failed successfully!`")
+        return message.reply_text(f"`Flashing` {text}.zip `failed successfully!`", telegram.ParseMode.MARKDOWN)
 
 __help__ = """
-Flash a file. Originally from nunobot++.
+Flash a file. Originally from NunoBot++.
 
 Usage: /flash <file>
 """
 
 __mod_name__ = "Flasher"
-
 ud_handle = DisableAbleCommandHandler("flash", flash)
-
 dispatcher.add_handler(ud_handle)
